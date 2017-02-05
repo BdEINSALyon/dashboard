5fe85c2859504b00ad0739b9031a3c714e6c9d17
Gabriel Augendre <gabriel@augendre.info>
2017-02-05 15:53:19 +0100

    Show verification paths for apps

--- board/static/board/script.js
+++ board/static/board/script.js
@@ -7,3 +7,7 @@ $('.show-ok').click(function (e) {
     e.preventDefault();
     $('.panel.panel-success').parent('div').show();
 });
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
