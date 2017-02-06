2e194ec4765939cc31ddccf0c8f364719a2a3fdd
Gabriel Augendre <gabriel@augendre.info>
2017-02-06 23:55:50 +0100

    Update and rename deploy-dashboard.sh to update-dashboard.sh

--- bash/deploy-dashboard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker pull bdeinsalyon/dashboard
-docker kill dashboard
-docker rm dashboard
-docker run --name dashboard --env-file /var/conf/environements/dashboard.env --volume /var/www/dashboard:/app/staticfiles -d bdeinsalyon/dashboard
---
+++ bash/update-dashboard.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+BASE_IMAGE="bdeinsalyon/dashboard"
+REGISTRY="registry.hub.docker.com"
+IMAGE="$REGISTRY/$BASE_IMAGE"
+CID=$(docker ps | grep $IMAGE | awk '{print $1}')
+docker pull $IMAGE
+
+for im in $CID
+do
+    LATEST=`docker inspect --format "{{.Id}}" $IMAGE`
+    RUNNING=`docker inspect --format "{{.Image}}" $im`
+    NAME=`docker inspect --format '{{.Name}}' $im | sed "s/\///g"`
+    echo "Latest:" $LATEST
+    echo "Running:" $RUNNING
+    if [ "$RUNNING" != "$LATEST" ];then
+        echo "upgrading $NAME"
+        docker stop $NAME
+        docker rm -f $NAME
+        docker run --name $NAME --env-file /var/conf/environements/$NAME.env --volume /var/www/$NAME:/app/staticfiles -d $IMAGE
+    else
+        echo "$NAME up to date"
+    fi
+done
